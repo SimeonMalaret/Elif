916087968f66316a83f49aa4c04e3dd7a491cabd
6meon <malaretsimeon@gmail.com>
2021-01-17 14:19:20 +0100

    Level Design Test Map

---
+++ Source/DreamHunters/LevelDesign_GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LevelDesign_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(minimalapi)
+class ALevelDesign_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ALevelDesign_GameMode();
+};
